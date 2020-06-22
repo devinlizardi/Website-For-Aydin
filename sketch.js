d63eb63d815ba977fb2bc91face719b60e99da5a
Devin Lizardi <dlizardi@berkeley.edu>
2020-06-22 14:12:32 -0700

    click info added
    Also added switch case support for specific text sequences, this could be fun to play with later

--- sketch.js
+++ sketch.js
@@ -2,8 +2,10 @@ var HEIGHT = 800;
 var WIDTH = 1400;
 var WORDS = ["love u bro", "stay healthy", "come on over!",
   "I'm actually really smart", "I identify as mixed race",
-  "If you have said the word “looting” more than you’ve said the names “George Floyd” or “Breonna Taylor” this week, then you are the problem. Period. Congratulations on allowing capitalism to own every last shred of your humanity. Innocent people are being systematically murdered by the police, the US government is trying to shut down free speech with tear gas and rubber bulvars, and you’re worried about how Target and Macy’s are doing? Fuck you. I would be happy to share some educational material with you if what’s going on right now is confusing to you, but the bottom line is so simple that everybody should be able to understand it - Black Lives Matter and fuck anyone who disagrees.", "let's smoke together", "I'm so high ahahaha", "Devin you should to try hash with me", "I think I'm the John Lennon of jericho", "when did i get so old guys", "*looks deeply into your eyes*", "*genuinely friendly smile*","it is SO nice to meet you breanna","can I get anyone coffee? No? Yes? \nOkay I will anyways", "*whimper while covered in water*","Have you ever done DMT?","want me to teach you guitar?","we HAVE to jam sometime","hi \n\n (this one from enrique)","somebody needed a pan?","yeah I'll come pick you up, it's only \n an hour away!","dude I totally understand what you mean!","do you need something? I can get it."
+  "If you have said the word “looting” more than you’ve said the names “George Floyd” or “Breonna Taylor” this week, then you are the problem. Period. Congratulations on allowing capitalism to own every last shred of your humanity. Innocent people are being systematically murdered by the police, the US government is trying to shut down free speech with tear gas and rubber bulvars, and you’re worried about how Target and Macy’s are doing? Fuck you. I would be happy to share some educational material with you if what’s going on right now is confusing to you, but the bottom line is so simple that everybody should be able to understand it - Black Lives Matter and fuck anyone who disagrees.", "let's smoke together", "I'm so high ahahaha", "Devin you should to try hash with me", "I think I'm the John Lennon of jericho", "when did i get so old guys", "*looks deeply into your eyes*", "*genuinely friendly smile*","it is SO nice to meet you breanna","can I get anyone coffee? No? Yes? \nOkay I will anyways", "*whimper while covered in water*","Have you ever done DMT?","want me to teach you guitar?","we HAVE to jam sometime","hi \n\n (this one from enrique)","somebody needed a pan?","yeah I'll come pick you up, it's only \n an hour away!","dude I totally understand what you mean!","do you need something? I can get it.","shall we smoke outside?","I am totally vibing"
 ];
+
+var word_sequence = ["Yeah, have some of these edibles Devin","haha they're really strong edibles be careful","oh shit man are you okay?","okay cool haha they're strong edibles for sure","Max and I are having a good time, sorry to hear you weren't bro!","take it easy brother"]
 
 var ayboi;
 var lovers;
@@ -69,7 +71,29 @@ function draw() {
 }
 
 function mousePressed() {
-  lovers.push(new Love());
+  switch(num_love) {
+  case 10:
+    lovers.push(new Love(word_sequence[0]))
+    break;
+  case 11:
+    lovers.push(new Love(word_sequence[1]))
+    break;
+  case 12:
+    lovers.push(new Love(word_sequence[2]))
+    break;
+  case 13:
+    lovers.push(new Love(word_sequence[3]))
+    break;
+  case 14:
+    lovers.push(new Love(word_sequence[4]))
+    break;
+  case 15:
+    lovers.push(new Love(word_sequence[5]))
+    break;
+  default: 
+    lovers.push(new Love(random(WORDS)));
+    break;
+  }
   if (!tripcounter) {
     bh_y += 2;
     num_love += 1;
@@ -77,7 +101,6 @@ function mousePressed() {
     bh_y -= 6;
     num_love += 2;
   }
-  print(num_love);
 }
 
 function areWeTrippin() {
@@ -99,8 +122,8 @@ function removeLove() {
 }
 
 class Love {
-  constructor() {
-    this.word = random(WORDS)
+  constructor(word) {
+    this.word = word;
     this.pos = createVector(WIDTH / 2, HEIGHT / 2)
     this.oldPosx = mouseX
     this.oldPosy = mouseY
